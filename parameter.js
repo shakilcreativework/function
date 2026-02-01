ab98c8c2d3df55ce9bb881e8f062637006e21259
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-01 12:46:01 +0600

    if(number === 0){         console.log('Please, give me greater then 0 --> zero');     }else if(!number){         console.log('Please, give me some value of numbers');     }else if(number < 0){         console.log('Please, give me positive value of numbers');     }else if(typeof(number) == 'string'){         console.log('Please, give me only number values');     }else{         console.log((number * number) - number);     }

--- parameter.js
+++ parameter.js
@@ -9,6 +9,10 @@ function square(number){
         console.log('Please, give me greater then 0 --> zero');
     }else if(!number){
         console.log('Please, give me some value of numbers');
+    }else if(number < 0){
+        console.log('Please, give me positive value of numbers');
+    }else if(typeof(number) == 'string'){
+        console.log('Please, give me only number values');
     }else{
         console.log((number * number) - number);
     }
@@ -16,4 +20,6 @@ function square(number){
 
 square(4);
 square(0);
+square(-1);
 square();
+square('ok');
