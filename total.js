3abb54e314fb89e4427d52b74e16a62eff584ba7
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-03 17:47:35 +0600

    amazing calcutate total shopping amount

---
+++ total.js
@@ -0,0 +1,20 @@
+const products = [
+    {name: 'shampoo', price: 300},
+    {name: 'chiruni', price: 100},
+    {name: 'shirt', price: 700},
+    {name: 'pant', price: 1200},
+];
+
+function getShoppingTotal(products){
+    // console.log(products);
+    let sum = 0;
+
+    for(const product of products){
+        // console.log(product);
+        sum = product.price + sum;
+    }
+    return sum;
+}
+
+const total = getShoppingTotal(products);
+console.log('total ajke khosabe: ', total);
